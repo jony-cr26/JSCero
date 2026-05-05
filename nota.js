d18f8ca3d95c5b2b955c43c985d07fed119182f8
jony-cr26 <jchavezr10@gmail.com>
2026-05-05 15:11:42 -0600

    Estructura if

---
+++ nota.js
@@ -0,0 +1,11 @@
+let nota = 59.9; // Puedes cambiar este valor para probar diferentes casos
+
+if (nota >= 90) {
+    console.log("Excelente");
+} else if (nota >= 75 && nota <= 89) {
+    console.log("Bien");
+} else if (nota >= 60 && nota <= 74) {
+    console.log("Suficiente");
+} else {
+    console.log("Insuficiente");
+}
